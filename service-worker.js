2dcbff397d055e86d47847b977fdc50c3e1b0729
DKV project <andrii.medvediev.ext@dkv-mobility.com>
2025-11-17 22:36:14 +0100

    Fix iOS PWA viewport height issue - add dynamic viewport calculation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'todo-app-v7.0';
+const CACHE_NAME = 'todo-app-v7.1';
 // Use empty BASE_PATH for local development, '/ToDo-App' for GitHub Pages
 const BASE_PATH = '';
 const urlsToCache = [
